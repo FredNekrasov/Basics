f8872285659b6245007e9418d98e132d8d3b8e1b
FredNekrasov <frednekrasov49@gmail.com>
2024-05-30 20:40:15 +0300

    creating a kotlin console project

---
+++ KotlinPL/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+group = "org.fredProjects.KotlinPL"
+version = "1.0-SNAPSHOT"
+repositories {
+    mavenCentral()
+}
+dependencies {
+    testImplementation(kotlin("test"))
+}
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
