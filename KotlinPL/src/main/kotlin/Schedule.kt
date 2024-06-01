1ce8bd3b778d3d987374057bf86f71842b8667cd
FredNekrasov <frednekrasov49@gmail.com>
2024-06-01 13:33:26 +0300

    creating a course plan

---
+++ KotlinPL/src/main/kotlin/Schedule.kt
@@ -0,0 +1,35 @@
+// start the course in the following order.
+// 1 beginning:
+// 1.1 DataTypes.kt
+// 1.2 IO.kt
+// 1.3 Arrays.kt
+// 1.4 Expressions.kt
+// 1.5 Loops.kt
+
+// 2 functions:
+// 2.1 Functions.kt
+// 2.2 Lambdas.kt
+
+// 3 collections:
+// 3.1 Lists.kt
+// 3.2 Maps.kt
+// 3.3 Sets.kt
+// 3.4 Sequences.kt
+
+// 4 strings:
+// 4.1 Strings.kt
+// 4.2 Regex.kt
+
+// 5 oop:
+// 5.1 Classes.kt
+// 5.2 Inheritance.kt
+// 5.3 Polymorphism.kt
+// 5.4 Interfaces.kt
+
+// 6 generics:
+// 6.1 GenericClasses.kt
+// 6.2 GenericFunctions.kt
+
+// 7 delegates:
+// 7.1 Delegates.kt
+// 7.2 PropertyDelegation.kt
